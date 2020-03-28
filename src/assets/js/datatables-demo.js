43b799c0d7555198fd4d0c2878e1ffa603545379
toppdogg42 <alo.cavali42@gmail.com>
2020-03-28 10:30:32 +0700

    3 files modified, 10 added

---
+++ src/assets/js/datatables-demo.js
@@ -0,0 +1,8 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable();
+});
+$(document).ready(function () {
+$('#dtBasicExample').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
